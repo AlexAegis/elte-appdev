4cc5424d207b3e5758d1c920c8bbb17e1905bde5
alexaegis <alexaegis@gmail.com>
2017-09-25 08:55:38 +0200

    asdee

---
+++ lesson03/src/main/java/elte/alexaegis/appdev/controller/UserController.java
@@ -0,0 +1,17 @@
+package elte.alexaegis.appdev.controller;
+
+import elte.alexaegis.appdev.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by alexaegis on 2017. 09. 25..
+ */
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+
+    public void asd() {
+        User user = new User();
+    }
+}
