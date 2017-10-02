31ffb06a434d8e912ddd575a8b72b5d9d3bb914c
alexaegis <alexaegis@gmail.com>
2017-10-02 09:28:48 +0200

    lesson04

--- lesson04/src/main/java/elte/alexaegis/appdev/controller/UserController.java
+++ lesson04/src/main/java/elte/alexaegis/appdev/controller/UserController.java
@@ -1,21 +1,26 @@
 package elte.alexaegis.appdev.controller;
 
 import elte.alexaegis.appdev.model.User;
-import elte.alexaegis.appdev.repository.UserRepository;
+import elte.alexaegis.appdev.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/user")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
-    @RequestMapping("/get")
-    public User get(@RequestParam("username") String username) {
-        return userRepository.findByUsername(username).get();
+    @RequestMapping(method = RequestMethod.POST, value = "/login")
+    public User login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        Optional<User> user = userService.login(username, password);
+        return user.get();
     }
+
 }
