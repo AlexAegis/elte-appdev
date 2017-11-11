0ca40155ab4acfa59b200fdf1e079e0a94b22cb5
Lehoczky <ifjlehoczkyzoltan@gmail.com>
2017-11-11 17:24:47 +0100

    Created person entity

---
+++ submission/src/main/java/elte/appdev/transactionmanager/model/Person.java
@@ -0,0 +1,35 @@
+package elte.appdev.transactionmanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PERSON")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Person extends ArchEntity {
+
+    @Column(nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column()
+    private String address;
+
+    @Column()
+    private String email;
+
+    @Column()
+    private String phone;
+}
