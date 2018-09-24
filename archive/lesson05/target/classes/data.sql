cbbbc3f5e0445c18c14ff9af8906ca573cbeffcd
Győri Sándor <alexaegis@gmail.com>
2018-09-24 15:49:54 +0200

    init root folder

---
+++ archive/lesson05/target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER (id, username, password) VALUES (1, 'admin', 'adminPw');
+INSERT INTO USER (id, username, password) VALUES (2, 'root', 'rootPw');
+INSERT INTO USER (id, username, password) VALUES (3, 'user', 'userPw');
