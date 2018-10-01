75d8fd80dc7a4677b987a572cfc4e95b26e5dd53
Győri Sándor <alexaegis@gmail.com>
2018-10-01 23:15:21 +0200

    added proper hot reload capable profile to the pom file, leaving the default profile intact to be able to be run anywhere

--- src/main/java/hu/elte/assignment/controller/HelloController.java
+++ src/main/java/hu/elte/assignment/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String index() {
-		return "Greetings from Sprin!";
+		return "Greetings from Sn!";
 	}
 
 	@RequestMapping("/hello2")
