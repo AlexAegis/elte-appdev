f3ccd25435a8e87f34319fbda00cfe4534674e48
Győri Sándor <alexaegis@gmail.com>
2018-10-01 22:50:48 +0200

    hot reload now also works from native maven run and vs code configuration

--- src/main/java/hu/elte/assignment/controller/HelloController.java
+++ src/main/java/hu/elte/assignment/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String index() {
-		return "Greetings from Spring Booasds!";
+		return "Greetings from Sprin!";
 	}
 
 	@RequestMapping("/hello2")
