bb11a912fe18156ddc311d206ed364fd4d2d0b84
Győri Sándor <alexaegis@gmail.com>
2018-10-07 13:44:40 +0200

    added basic entity and a few example entry, a repository and a controller
    Former-commit-id: 23fd007692297419e03b9b2c6409d20bfeddb906

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user (username, password) values ('admin', 'admin');
+insert into user (username, password) values ('test', 'test');
+insert into user (username, password) values ('backup', 'backup');
