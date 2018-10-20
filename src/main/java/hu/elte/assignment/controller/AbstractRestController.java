f0bf250bf7f3f4b638018f742f3074ef029cbf26
Győri Sándor <alexaegis@gmail.com>
2018-10-20 19:31:36 +0200

    added angular login, not complete

--- src/main/java/hu/elte/assignment/controller/AbstractRestController.java
+++
@@ -1,7 +0,0 @@
-package hu.elte.assignment.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("rest")
-public abstract class AbstractRestController {
-}
