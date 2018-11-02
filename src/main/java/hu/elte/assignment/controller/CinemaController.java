e0e441fdd4ca2b36ed022727290c424ea4b86daf
Győri Sándor <alexaegis@gmail.com>
2018-11-03 00:23:28 +0100

    fixed findbyToken

--- src/main/java/hu/elte/assignment/controller/CinemaController.java
+++ src/main/java/hu/elte/assignment/controller/CinemaController.java
@@ -9,9 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
 import static lombok.AccessLevel.PRIVATE;
 
