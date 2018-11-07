a68d02eeed0e808faa36b7b6eb48b83983166344
Győri Sándor <alexaegis@gmail.com>
2018-11-07 18:48:52 +0100

    Autorefresh interceptor implemented

--- src/main/web/src/app/interceptors/refresh-token.interceptor.ts
+++ src/main/web/src/app/interceptors/refresh-token.interceptor.ts
@@ -1,23 +1,16 @@
-import { Injectable, Inject, Optional } from '@angular/core';
-import { OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
-import {
-	HttpEvent,
-	HttpHandler,
-	HttpInterceptor,
-	HttpRequest,
-	HttpResponse,
-	HttpErrorResponse
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { url } from 'inspector';
+import { Injectable } from '@angular/core';
 
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 @Injectable({ providedIn: 'root' })
 export class RefreshTokenInterceptor implements HttpInterceptor {
-	constructor(private authStorage: OAuthStorage) {}
+	constructor(private auth: AuthService) {}
 
 	public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		// Interceptor Example
-		// console.log(req.url);
+		if (!req.url.startsWith('oauth')) {
+			this.auth.refresh();
+		}
 		return next.handle(req);
 	}
 }
