f0ce4ac3251abf921b2c18ae2e56dc908616ea1b
Győri Sándor <alexaegis@gmail.com>
2018-11-09 18:24:56 +0100

    renamed folders

--- src/main/web/src/app/app.module.ts
+++ src/main/web/src/app/app.module.ts
@@ -3,25 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { RoutingModule } from './router/routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './components/hello/hello.component';
-import { LoginComponent } from './components/login/login.component';
+import { HelloComponent } from './component/hello/hello.component';
+import { LoginComponent } from './component/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthService } from './service/auth.service';
 import { ReactiveFormsModule } from '@angular/forms';
-import { UserComponent } from './components/user/user.component';
-import { LoadingComponent } from './components/loading/loading.component';
+import { UserComponent } from './component/user/user.component';
+import { LoadingComponent } from './component/loading/loading.component';
 import { LoadingDirective } from './directives/loading.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './modules/material.module';
+import { MaterialModule } from './module/material.module';
 import { MomentModule } from 'ngx-moment';
 import { NgProgressModule } from '@ngx-progressbar/core';
 import { NgProgressHttpModule } from '@ngx-progressbar/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
-import { RefreshTokenInterceptor } from './interceptors/refresh-token.interceptor';
-import { HolaComponent } from './dummy/hola/hola.component';
-import { HaloComponent } from './dummy/halo/halo.component';
+import { RefreshTokenInterceptor } from './interceptor/refresh-token.interceptor';
+import { HolaComponent } from './component/dummy/hola/hola.component';
+import { HaloComponent } from './component/dummy/halo/halo.component';
 import { AvatarModule } from 'ngx-avatar';
+import { RegisterComponent } from './component/register/register.component';
 @NgModule({
 	entryComponents: [LoadingComponent],
 	declarations: [
@@ -32,7 +33,8 @@ import { AvatarModule } from 'ngx-avatar';
 		LoadingComponent,
 		LoadingDirective,
 		HolaComponent,
-		HaloComponent
+		HaloComponent,
+		RegisterComponent
 	],
 	imports: [
 		BrowserModule,
