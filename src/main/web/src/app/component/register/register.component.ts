d32ac69656a1aa17c5f19c2c8e5e1d5cceb53151
Győri Sándor <alexaegis@gmail.com>
2018-11-10 12:34:49 +0100

    almost what I want

--- src/main/web/src/app/component/register/register.component.ts
+++ src/main/web/src/app/component/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { ActivatedRoute } from '@angular/router';
+import { slideInAnimation } from 'src/app/animation/route.animation';
 
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.component.html',
-	styleUrls: ['./register.component.scss']
+	styleUrls: ['./register.component.scss'],
+	animations: [slideInAnimation]
 })
 export class RegisterComponent implements OnInit {
-	constructor() {}
+	shouldAnimate: boolean;
+	constructor(private route: ActivatedRoute) {
+		console.log(`asd ${this.route.outlet}`);
+		if (this.route.outlet === 'register') {
+			this.shouldAnimate = true; // Yes, I do enter here every time
+		}
+	}
 
 	ngOnInit() {}
 }
