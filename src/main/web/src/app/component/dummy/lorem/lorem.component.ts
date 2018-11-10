b1fb8b8ca2b4ced806a964c2bfba1458f5148e10
Győri Sándor <alexaegis@gmail.com>
2018-11-10 14:49:39 +0100

    getting there

---
+++ src/main/web/src/app/component/dummy/lorem/lorem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lorem',
+  templateUrl: './lorem.component.html',
+  styleUrls: ['./lorem.component.scss']
+})
+export class LoremComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
